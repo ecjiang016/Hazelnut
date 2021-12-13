a1f5cbbfbd48cdcc903d9d3f4314c1bbad9cd29e
ecjiang016 <ecjiang016@gmail.com>
2021-12-12 20:43:03 -0600

    Cleaned up CNN backpropagation code

--- convolution.py
+++ convolution.py
@@ -1,10 +1,10 @@
 import numpy as np
-from scipy.signal import convolve as Conv
+from scipy.signal import fftconvolve as Conv
 from .ActivationFunctions import *
 
 class convolution:
     def __init__(self, Layout):
-        self.Layout = Layout
+        self.Layout = Layout #Layout = [("Kernel", kernel), ("Acti_func", "ReLU"), ("Pooling", size)]
     
     def Pool(self, matrix, size):
         strides_y = int(np.floor(matrix.shape[0]/size))
@@ -34,22 +34,22 @@ class convolution:
         return output
 
     def FeedForward(self, input_array):
-        history = [("Input", input_array)]
+        history = [input_array]
         for module in self.Layout:
             module_type = module[0]
-            activations = history[-1][1]
+            activations = history[-1]
 
             if module_type == "Kernel":
                 output = Conv(activations, module[1], "valid")
-                history.append((module_type, output))
+                history.append(output)
             
             elif module_type == "Pooling":
                 output, mask = self.Pool(activations, module[1])
-                history.append((module_type, output, mask))
+                history.append((output, mask))
                 
             elif module_type == "Acti_func":
                 output = AF(activations, module[1])
-                history.append((module_type, output))
+                history.append(output)
             
             else:
                 raise ValueError("Invalid convolution module layout")
@@ -62,49 +62,28 @@ class convolution:
             if module[0] == "Kernel":
                 kernel_gradients[i] = 0
 
-        for example_num, previous_gradient in enumerate(PreviousGradient):
-            history = History[example_num]
-            for j, module in enumerate(reversed(self.Layout)):
-                i = 0-j-1
+        last = len(self.Layout) -1 #Reduces amount of len() calls
+
+        for history, previous_gradient in zip(History, PreviousGradient):
+            for index, module in enumerate(reversed(self.Layout)):
+
+                i = last - index #Compromises for the reversing of the layout but not the index
                 module_type = module[0]
 
                 if module_type == "Kernel":
                     flipped_kernel = np.flipud(np.fliplr(module[1]))
-                    index = len(self.Layout)+i
-                    kernel_gradients[index] += Conv(history[i-1][1], previous_gradient, "valid")
+
+                    kernel_gradients[i] += Conv(history[i], previous_gradient, "valid")
                     previous_gradient = Conv(flipped_kernel, previous_gradient, "full")
                     
                 elif module_type == "Pooling":
-                    previous_gradient = self.Pool_Backpropagate(previous_gradient, history[i][2], history[i-1][1].shape, module[1])
+                    previous_gradient = self.Pool_Backpropagate(previous_gradient, history[i+1][1], history[i][0].shape, module[1])
 
                 elif module_type == "Acti_func":
-                    previous_gradient = previous_gradient * AF_dv(history[i][1], module[1])
+                    previous_gradient = previous_gradient * AF_dv(history[i], module[1])
 
                 else:
                     raise ValueError("Invalid convolution module layout")
         
         for index, gradient in kernel_gradients.items():
-            self.Layout[index] = ("Kernel", self.Layout[index][1] + ((gradient/len(PreviousGradient)) * learning_rate))
-
-
-    def Backpropagate_single(self, history, PreviousGradient, learning_rate):
-        previous_gradient = PreviousGradient
-        
-        for j, module in enumerate(reversed(self.Layout)):
-            i = 0-j-1
-            module_type = module[0]
-
-            if module_type == "Kernel":
-                flipped_kernel = np.flipud(np.fliplr(module[1]))
-                gradient = Conv(history[i-1][1], previous_gradient, "valid")
-                self.Layout[i] = ("Kernel", module[1] - (gradient * learning_rate))
-                previous_gradient = Conv(flipped_kernel, previous_gradient, "full")
-                
-            elif module_type == "Pooling":
-                previous_gradient = self.Pool_Backpropagate(previous_gradient, history[i][2], history[i-1][1].shape, module[1])
-
-            elif module_type == "Acti_func":
-                previous_gradient = previous_gradient * AF_dv(history[i][1], module[1])
-
-            else:
-                raise ValueError("Invalid convolution module layout")
+            self.Layout[index] = ("Kernel", self.Layout[index][1] - ((gradient/len(PreviousGradient)) * learning_rate))
