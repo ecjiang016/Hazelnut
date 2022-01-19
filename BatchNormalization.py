Bug fixes for optimizers and batch normalization

BatchNormalization.py
@@ -24,7 +24,7 @@ def FeedForward(inp, parameters, training=False):
         for n in range(N):
             for c in range(C):
                 mean[n, c] = np.sum(inp[n, c]) / channel_size
-                variance[n, c] = np.square(inp - mean[n, c]) / channel_size
+                variance[n, c] = np.sum(np.square(inp - mean[n, c])) / channel_size
     
     else:
         mean = test_mean
@@ -61,7 +61,7 @@ def Backpropagate(grad, parameters, cache, learning_rate): #Need to implement op
     cache0, cache1, cache2 = cache
 
     N, C, H, W = grad.shape
-    gamma, beta, _, _ = parameters
+    gamma, beta, test_mean, test_variance = parameters
 
     beta_grad = sum(grad)
     gamma_grad = sum(grad * cache2)
@@ -73,7 +73,7 @@ def Backpropagate(grad, parameters, cache, learning_rate): #Need to implement op
     for n in range(N):
         for c in range(C):
             variance_grad = np.sum(cache0[n, c] * (-(gamma[c] * (cache1[n, c]**3))/2))
-            mean_grad = np.sum(((-(gamma[c] * cache1[n, c])/2) * grad[n, c])) + ((-2 * variance_grad[c] * np.sum(cache0[n, c]))/C)
+            mean_grad = np.sum(((-(gamma[c] * cache1[n, c])/2) * grad[n, c])) + ((-2 * variance_grad * np.sum(cache0[n, c]))/C)
             pass_gradient[n, c] = (grad[n, c] * cache1[n, c]) + ((variance_grad * 2 * cache0[n, c])/C) + (mean_grad/C)
     
-    return pass_gradient
+    return pass_gradient, (gamma, beta, test_mean, test_variance)

convolution.py
@@ -46,29 +46,42 @@ class convolution:
         return output
 
     def FeedForward(self, input_array):
-        history = [input_array]
+        history = []
         activations = input_array
         for module in self.Layout:
             module_type = module[0]
 
             if module_type == "Filter":
+                history.append(activations)
+
                 if module[2] == "Same":
-                    activations = np.pad(activations, (module[1].shape - 1) //2)
-                history.append(conv(activations, module[1]))
+                    N, C, H, W = activations.shape
+                    padded_activations = np.zeros((N, C, H + module[1].shape[2] - 1, W + module[1].shape[2] - 1))
+                    for n in range(N):
+                        for c in range(C):
+                            padded_activations[n, c] = np.pad(activations[n, c], (module[1].shape[2] - 1) //2)
+                    activations = padded_activations
+
+                    activations = np.array(padded_activations)
+
+                activations = conv(activations, module[1])
+
 
             elif module_type == "Batch Norm":
                 activations, cache = BatchNorm.FeedForward(activations, module[1], training=self.Training)
                 history.append(cache)
                 
             elif module_type == "AF":
-                activations = AF(activations, module[1])
                 history.append(activations)
+                activations = AF(activations, module[1])
 
             elif module_type == "Skip":
                 if module[1] == "Begin":
                     skip_cache = activations
                 else:
                     activations += skip_cache
+
+                history.append(None)
 
             elif module_type == "Pooling":
                 output, mask = self.Pool(activations, module[1])
@@ -90,25 +103,44 @@ class convolution:
             module_type = module[0]
 
             if module_type == "Filter":
-                acti = np.swapaxes(history[i], 0, 1)
+                acti = history[i]
+                if module[2] == "Same":
+                    N, C, H, W = pass_gradient.shape
+                    padded_acti = np.zeros((N, C, H+module[1].shape[2] - 1, W+module[1].shape[2] - 1))
+
+                    for n in range(N):
+                        for c in range(C):
+                            padded_acti[n, c] = np.pad(acti[n, c], (module[1].shape[2] -1) //2)
+
+                    acti = padded_acti
+                    
+                acti = np.swapaxes(acti, 0, 1)
                 #Calculate and update kernel gradients
                 grad = np.swapaxes(pass_gradient, 0, 1)
                 kernel_gradients = np.swapaxes(conv(acti, grad), 0, 1)/batch_size
 
                 if module[2] == "Same":
-                    padding_size = (module[1].shape -1) //2
+                    padding_size = (module[1].shape[2] -1) //2
                     kernel_gradients = kernel_gradients[:, :, padding_size:-padding_size, padding_size:-padding_size]
-                    pass_gradient = np.pad(pass_gradient, padding_size)
+                    
+                    N, C, H, W = pass_gradient.shape
+                    padded_gradient = np.zeros((N, C, H+module[1].shape[2] - 1, W+module[1].shape[2] - 1))
+                    for n in range(N):
+                        for c in range(C):
+                            padded_gradient[n, c] = np.pad(pass_gradient[n, c], padding_size)
 
-                kernel_gradients, self.OptimizerCache[i] = self.Optimizer(self.LearningRate, kernel_gradients, self.OptimizerCache[i])
-                self.Layout[i] = ("Filter", self.Layout[i][1] - kernel_gradients)
+                    pass_gradient = padded_gradient
 
                 #Calculate the next gradient
                 new_kern = np.flip(np.swapaxes(module[1], 0, 1), (2, 3))
                 pass_gradient = conv_full(pass_gradient, new_kern)
 
-            elif module_type == "BatchNorm":
-                pass_gradient, module[1] = ("BatchNorm", BatchNorm.Backpropagate(pass_gradient, module[1], history[i], self.LearningRate))
+                kernel_gradients, self.OptimizerCache[i] = self.Optimizer(self.LearningRate, kernel_gradients, self.OptimizerCache[i])
+                self.Layout[i] = ("Filter", self.Layout[i][1] - kernel_gradients)
+
+            elif module_type == "Batch Norm":
+                pass_gradient, parameters = BatchNorm.Backpropagate(pass_gradient, module[1], history[i], self.LearningRate)
+                module = ("Batch Norm", parameters)
 
             elif module_type == "AF":
                 pass_gradient = pass_gradient * AF_dv(history[i], module[1])

mlp.py
@@ -57,6 +57,8 @@ class mlp:
         return activations, z_vectors
         
     def Backpropagate(self, activations, z_vectors, desired_output, training_batch, pass_on_gradient=False):
+        self.Biases_mat = self.Bias_convert(training_batch)
+
         ErrorVector = [AF_dv(z_vectors[self.LastLayer], self.AF_Type[self.LastLayer])*self.cost_function(activations[self.LastLayer], desired_output)]
         for j in range(1, self.LastLayer):
             i = self.LastLayer - j
@@ -66,12 +68,12 @@ class mlp:
         #Update matrices
         for i in range(1, self.LastLayer+1):
             weight_gradient = np.matmul(ErrorVector[i], activations[i-1].T) / training_batch
-            Grad, self.OptimizerCache = self.Optimizer(self.LearningRate, weight_gradient, self.OptimizerCache[i-1])
+            Grad, self.OptimizerCache[i-1] = self.Optimizer(self.LearningRate, weight_gradient, self.OptimizerCache[i-1])
             self.Weights[i] = self.Weights[i] - Grad
 
         for i in range(1, self.LastLayer+1):
             bias_gradient = np.matmul(ErrorVector[i], np.full((training_batch, training_batch), 1)) / training_batch
-            Grad, self.OptimizerCache = self.Optimizer(self.LearningRate, bias_gradient, self.OptimizerCache[i-1 + self.LastLayer])
+            Grad, self.OptimizerCache[i-1 + self.LastLayer] = self.Optimizer(self.LearningRate, bias_gradient, self.OptimizerCache[i-1 + self.LastLayer])
             self.Biases_mat[i] = self.Biases_mat[i] - Grad
         
         if pass_on_gradient == True: