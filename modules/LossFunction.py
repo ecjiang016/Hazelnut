2a520ce0f24ed89d77722735280dc9ac2142558d
Evan Jiang <ecjiang016@gmail.com>
2022-02-24 14:13:02 -0600

    Fixed classes

--- modules/LossFunction.py
+++ modules/LossFunction.py
@@ -4,21 +4,27 @@ class MSE:
     """
     Mean squared error
     """
-    def Forward(out, correct_out):
+    def __init__(self) -> None:
+        pass
+
+    def Forward(self, out, correct_out):
         return np.sum(np.square(out-correct_out)) / out.shape[0]
 
-    def Backward(out, correct_out):
+    def Backward(self, out, correct_out):
         return out - correct_out
 
 class CrossEntropy:
     """
     Note: Also does a softmax before applying the crosss entropy loss
     """
-    def Forward(out, correct_out):
+    def __init__(self) -> None:
+        pass
+
+    def Forward(self, out, correct_out):
         #Softmax time
         exps = np.exp(out - np.max(out, axis=0)) #Add numerical stability
         softmax_out = exps/np.sum(exps, axis=0)
         return np.sum(correct_out * np.log(softmax_out)) / out.shape[0]
 
-    def Backward(out, correct_out):
+    def Backward(self, out, correct_out):
         return out - correct_out
