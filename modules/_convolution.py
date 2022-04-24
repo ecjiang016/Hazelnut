Changed saving and fixed gradient normalization

modules/_convolution.py
@@ -81,7 +81,7 @@ class Conv:
             )
             
         #inp reshape: (N, F, OH, OW) -> (OW, OH, N, F)
-        filter_grad = self.np.tensordot(cache_acti_col, inp.transpose(3, 2, 0, 1), axes=3)
+        filter_grad = self.np.tensordot(cache_acti_col, inp.transpose(3, 2, 0, 1), axes=3) / inp.shape[0]
         self.filter -= self.optimizer.use(filter_grad)
         
         if self.PAD:
@@ -125,7 +125,7 @@ class Conv:
         self.PFW = self.KS + self.PAD_F_SIZE_W + self.PAD_F_SIZE_W
 
     def Save(self):
-        return {'args':(self.F, self.KS, self.mode, self.init_method), 'var':(self.filter, self.optimizer.__class__, self.optimizer.Save())}
+        return {'args':(self.F, self.KS, self.mode), 'var':(self.filter, self.optimizer.__class__, self.optimizer.Save())}
 
     def Load(self, var):
         self.filter, optimizer_class, optimizer_dict = var

modules/_linear_layer.py
@@ -34,8 +34,8 @@ class Linear:
         return self.np.matmul(self.weights, inp) + self.biases[:, None]
 
     def Backward(self, inp):
-        self.weights -= self.optimizer_weights.use(self.np.matmul(inp, self.training_cache.T) / inp.shape[0])
-        self.biases -= self.optimizer_biases.use(self.np.sum(inp, axis=1) / inp.shape[0])
+        self.weights -= self.optimizer_weights.use(self.np.matmul(inp, self.training_cache.T) / inp.shape[1])
+        self.biases -= self.optimizer_biases.use(self.np.sum(inp, axis=1) / inp.shape[1])
         return self.np.matmul(self.weights.T, inp)
 
     def Build(self, shape):
@@ -60,7 +60,7 @@ class Linear:
         self.biases = self.np.array(self.biases) 
 
     def Save(self):
-        return {'args':(self.Neurons, self.init_method), 'var':(self.weights, self.biases, self.optimizer.__class__, self.optimizer.Save())}
+        return {'args':(self.Neurons), 'var':(self.weights, self.biases, self.optimizer.__class__, self.optimizer.Save())}
 
     def Load(self, var):
         self.weights, self.biases, optimizer_class, optimizer_dict = var