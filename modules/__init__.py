af3ba4d4b92f45d1b6f82ec3bc5153afc5a63ff6
ecjiang016 <95730592+ecjiang016@users.noreply.github.com>
2022-04-27 14:32:21 -0500

    Added SkipConnClass

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,3 +6,4 @@ from ._convolution import Conv
 from ._batch_normalization import BatchNorm
 from ._linear_layer import Linear
 from ._skip_connection import SkipConn
+from ._skip_connection import SkipConnClass
