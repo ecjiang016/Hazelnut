b7768d9d44b62de09e47b7ccaccb732df574cca9
Evan Jiang <ecjiang@bluevalleyk12.net>
2022-05-01 19:53:16 -0500

    Closer to working Yellowfin

--- presets/YellowFin.py
+++ presets/YellowFin.py
@@ -5,7 +5,7 @@ class NN(Base_NN):
     """
     Neural net with YellowFin
     """
-    def __init__(self):
+    def __init__(self, beta=0.999, slow_start=True, gradient_clip=True, sliding_window=20):
         """
         Make sure to super().__init__()
 
@@ -14,11 +14,17 @@ class NN(Base_NN):
         - self.epsilon: For numerical stability. Default value 1e-7
         """
         super().__init__()
-        self.optimizer = SGDM
+        self.optimizer = SGDM()
 
         #YellowFin variables
-        self.beta = 0.9
+        self.beta = beta
         self.epsilon = 1e-7
+        self.slow_start = slow_start
+        self.gradient_clip = gradient_clip
+        self.sliding_window = sliding_window
+
+        #Iteration tracking
+        self.t = 0
 
         #Curvature range variables
         self.h = []
@@ -33,50 +39,65 @@ class NN(Base_NN):
         self.g_norm = 0
         self.h_ = 0
         self.D = 0
-    
-    def train(self, inp, correct_out):
-        out = self.forward_train(inp)
-        back_gradient = self.loss.Backward(out, correct_out)
-        gradient = self.np.sum(back_gradient, axis=1)[:, None] / back_gradient.shape[1]
 
-        #Calculate learning rate and momentum weight
+    def update(self):
+        grad = []
+        for module in self.layout:
+            try:
+                module_gradient = module.gradient
+                if type(module_gradient) is tuple:
+                    for gradient in module_gradient:
+                        grad.append(self.np.ravel(gradient))
+
+                else:
+                    grad.append(self.np.ravel(module_gradient))
+            except AttributeError:
+                pass
+    
+        grad = self.np.concatenate(grad)
+        grad_norm2 = float(self.np.square(grad).sum())
 
         #Calculate curvature range
-        self.h.append(float(self.np.sum(self.np.square(gradient))))
-        if len(self.h) > 50:
-            self.h.pop(0)
+        self.h.append(grad_norm2)
+        if len(self.h) > self.sliding_window:
+            del self.h[0]
 
         h_max_t = max(self.h)
         h_min_t = min(self.h)
 
-        self.h_max = (self.beta * self.h_max) + ((1 - self.beta) * h_max_t)
+        if self.gradient_clip:
+            self.h_max = (self.beta * self.h_max) + ((1 - self.beta) * min(h_max_t, 100 * self.h_max))
+        else:
+            self.h_max = (self.beta * self.h_max) + ((1 - self.beta) * h_max_t)
         self.h_min = (self.beta * self.h_min) + ((1 - self.beta) * h_min_t)
 
         #Gradient variance
-        self.g_2 = (self.beta * self.g_2) + ((1 - self.beta) * self.np.square(gradient))
-        self.g_ = (self.beta * self.g_) + ((1 - self.beta) * gradient)
+        self.g_2 = (self.beta * self.g_2) + ((1 - self.beta) * self.np.square(grad))
+        self.g_ = (self.beta * self.g_) + ((1 - self.beta) * grad)
         C = float(self.np.sum(self.g_2 - self.np.square(self.g_)))
 
         #Distance to optimum
-        self.g_norm = (self.beta * self.g_norm) + ((1 - self.beta) * float(self.np.sum(self.np.absolute(self.g_norm))))
-        self.h_ = (self.beta * self.h_) + ((1 - self.beta) * self.h[-1])
+        self.g_norm = (self.beta * self.g_norm) + ((1 - self.beta) * self.np.sqrt(grad_norm2))
+        self.h_ = (self.beta * self.h_) + ((1 - self.beta) * grad_norm2)
         self.D = (self.beta * self.D) + ((1 - self.beta) * (self.g_norm/self.h_))
-
 
         #SingleStep
         
         #Get cubic root
         #From https://github.com/JianGoForIt/YellowFin/blob/master/tuner_utils/yellowfin.py
-        p = (self.D + self.epsilon)**2 * (self._h_min + self.epsilon)**2 / 2 / (self._grad_var + self.epsilon)
+        p = (self.D + self.epsilon)**2 * (self.h_min + self.epsilon)**2 / 2 / (C + self.epsilon)
         w3 = (-self.np.sqrt(p**2 + 4.0 / 27.0 * p**3) - p) / 2.0
-        w = self.np.sign(w3) * self.np.pow(self.np.abs(w3), 1.0/3.0)
+        w = self.np.sign(w3) * self.np.power(self.np.abs(w3), 1.0/3.0)
         y = w - p / 3.0 / (w + self.epsilon)
         x = y + 1
 
-        root = self.get_cubic_root() ** 2
-        dr = self.np.maximum( (self._h_max + self.epsilon) / (self._h_min + self.epsilon), 1.0 + self.epsilon)
-        learning_rate = self.np.maximum(root, ((self.np.sqrt(dr) - 1) / (self.np.sqrt(dr) + 1))**2)
-        momentum = self.np.square(1 - self.np.sqrt(learning_rate)) / self.h_min
+        root = x ** 2
+        dr = self.np.maximum( (self.h_max + self.epsilon) / (self.h_min + self.epsilon), 1.0 + self.epsilon)
+        momentum = self.np.maximum(root, ((self.np.sqrt(dr) - 1) / (self.np.sqrt(dr) + 1))**2)
+        learning_rate = self.np.square(1 - self.np.sqrt(momentum)) / self.h_min
+
+        if self.slow_start:
+            learning_rate = min(learning_rate, self.t * learning_rate / (10 * self.sliding_window))
 
         #Update all the optimizers
         for module in self.layout:
@@ -86,10 +107,6 @@ class NN(Base_NN):
             except AttributeError: #Module doesn't have an optimizer
                 pass
 
-        self.backpropagate(back_gradient)
-        loss = self.loss.Forward(out, correct_out)
-
-        if self.np.isnan(loss):
-            raise RuntimeError("Loss is NaN")
-
-        return loss, out
+        self.t += 1
+        
+        super().update()
